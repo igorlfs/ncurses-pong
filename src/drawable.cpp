f5fafa288baf221311fe06b542adc3281262fe64
igorlfs <igorlfs@ufmg.br>
2021-11-25 09:59:19 -0300

    doc: Drawable

--- src/drawable.cpp
+++ src/drawable.cpp
@@ -2,6 +2,7 @@
 
 using namespace drawable;
 
+/// @return first body position if defined or else head
 pair<int, int> Drawable::getHead() const {
     if (this->body)
         return this->body[0];
@@ -9,6 +10,7 @@ pair<int, int> Drawable::getHead() const {
         return this->head;
 }
 
+/// @return last body position if defined or else head
 pair<int, int> Drawable::getTail() const {
     if (this->body)
         return this->body[this->bodySize - 1];
@@ -16,11 +18,13 @@ pair<int, int> Drawable::getTail() const {
         return this->head;
 }
 
+/// @brief changes head position to p and also updates prevHead
 void Drawable::setHead(const pair<int, int> &p) {
     this->prevHead = this->head;
     this->head = p;
 }
 
+/// @brief returns true if p is inside body. Always false if size = 0
 bool Drawable::isInsideBody(const pair<int, int> &p) const {
     for (int i = 0; i < this->bodySize; ++i)
         if (p == this->body[i]) return true;
@@ -32,6 +36,7 @@ void Drawable::printHead(board::Board b) const {
     b.print(this->head.first, this->head.second, this->character);
 }
 
+/// @brief prints body to b, if defined, or else does nothing
 void Drawable::printBody(board::Board b) const {
     if (this->bodySize == 0) return;
     for (int i = 0; i < this->bodySize; ++i) {
