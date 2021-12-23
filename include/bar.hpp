5fd07a6ae1b46cc9e08341dac00e40f67faa7a9d
Igor <igorlfs@ufmg.br>
2021-12-23 17:00:37 -0300

    Revert "fixup: move getTail to Drawable"
    This reverts commit f06f8b30f70028d56593a252ac232d15cda09dac.

--- include/bar.hpp
+++ include/bar.hpp
@@ -10,6 +10,7 @@ static constexpr char BAR_CH = '|';
 class bar : public Drawable {
   public:
     bar(const int &x);
+    pair<int, int> getTail() const { return this->body[SIZE - 1]; }
     pair<int, int> moveUp();
     pair<int, int> moveDown();
 
